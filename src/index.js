5535466f044bd7542cfea058b6c1cbdbc64cd844
omsf <ocimar.acad@gmail.com>
2020-06-24 21:47:06 -0300

    Use GLSL to map panter texture

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ let scene = new THREE.Scene();
 scene.fog = new THREE.Fog(0x787e74, 0.1, 60);
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+// RENDER
 let renderer = new THREE.WebGLRenderer({antialias: true});
 // renderer.setClearColor(0x87CEEB, 1); //-> This is the blue sky color
 renderer.setClearColor(0x787e74, 1);
@@ -18,6 +19,7 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
+// GROUND
 // let texture = THREE.ImageUtils.loadTexture("./src/grass.jpg");
 let texture = THREE.ImageUtils.loadTexture("./src/ground.jpg");
 texture.wrapS = THREE.RepeatWrapping;
@@ -54,6 +56,7 @@ planeGeometry.rotateX( - Math.PI / 2);
 
 scene.add(plane);
 
+// LIGHT
 let ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
 scene.add(ambientLight);
 let light = new THREE.PointLight(0xffffff);
@@ -65,12 +68,37 @@ light.shadow.mapSize.width = 2048;
 light.shadow.mapSize.height = 2048;
 scene.add(light);
 
+// CONTROLS
 let controls = new OrbitControls(camera, renderer.domElement);
 camera.position.y = 10;
 camera.position.x = 0;
 camera.position.z = 15;
 
 controls.update();
+
+// // SKYBOX
+// let materialArray = [
+//     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./src/skybox/negx.jpg'),
+//         side: THREE.DoubleSide}),
+//     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./src/skybox/negy.jpg'),
+//         side: THREE.DoubleSide}),
+//     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./src/skybox/negz.jpg'),
+//         side: THREE.DoubleSide}),
+//     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./src/skybox/posx.jpg'),
+//         side: THREE.DoubleSide}),
+//     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./src/skybox/posy.jpg'),
+//         side: THREE.DoubleSide}),
+//     new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load('./src/skybox/posz.jpg'),
+//         side: THREE.DoubleSide})
+// ]
+//
+// let skyboxGeo = new THREE.BoxGeometry( 10000, 10000, 10000);
+// let skyboxMat = new THREE.MeshFaceMaterial(materialArray)
+// // let skybox = new THREE.Mesh( skyboxGeo, skyboxMat );
+// let skybox = new THREE.Mesh( skyboxGeo, materialArray );
+// scene.add( skybox );
+
+// MODELS
 const modelsPath = "./src/models/";
 
 let grass = new Grass(modelsPath);
@@ -83,11 +111,12 @@ let smallTree = new SmallTree(modelsPath);
 smallTree.load(scene);
 
 let panther = new Panther(modelsPath);
-panther.load(scene);
+panther.load(scene, document);
 
 let capybara = new Capybara(modelsPath);
 capybara.load(scene);
 
+// ANIMATE FUNCTION
 var animate = function () {
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
